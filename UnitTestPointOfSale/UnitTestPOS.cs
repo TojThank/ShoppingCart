41b5b78ca7585cfe5beb0c59fd1784b3d2f9822c
TojThank <toji.pv@hotmail.com>
2019-07-31 17:18:38 +0100

    Modified test method to check muliple scenarios

--- UnitTestPointOfSale/UnitTestPOS.cs
+++ UnitTestPointOfSale/UnitTestPOS.cs
@@ -1,16 +1,32 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShoppingCart;
+using System.Collections.Generic;
 using System.Linq;
 namespace UnitTestPointOfSale
 {
     [TestClass]
     public class UnitTestPOS
     {
+        
         string[] items = { "Apple", "Apple", "Orange", "Apple" };
-        PointOfSale Pos;
+        string[] items1 = { "Apple", "Orange", "Orange", "Orange" };
+        string[] items2 = { "Apple", "Apple", "Orange", "Orange" };
+        string[] items3 = { "Orange", "Orange", "Orange", "Orange" };
+        string[] items4 = { "Apple", "Apple", "Apple", "Apple" };
+
+        string[] items5 = { "Apple", "Orange", "Orange", "Orange", "Orange", "Orange", "Orange", "Apple", "Apple", "Apple", "Apple" };
+        IDictionary<PointOfSale,double> PosSingleItem;
+        IDictionary<PointOfSale, double> PosMultiItem;
         public UnitTestPOS()
         {
-            Pos = new PointOfSale(items);
+            PosMultiItem = new Dictionary<PointOfSale, double>();
+            PosSingleItem = new Dictionary<PointOfSale, double>();
+            PosMultiItem.Add(new PointOfSale(items),1.45);
+            PosMultiItem.Add(new PointOfSale(items1), 1.10);
+            PosMultiItem.Add(new PointOfSale(items2), 1.10);
+            PosSingleItem.Add(new PointOfSale(items3), 0.75);
+            PosSingleItem.Add(new PointOfSale(items4), 1.20);
+            PosMultiItem.Add(new PointOfSale(items5), 2.80);
         }
 
         /// <summary>
@@ -19,7 +35,17 @@ namespace UnitTestPointOfSale
         [TestMethod]
         public void TestPOSBillItemCount()
         {
-            Assert.IsTrue(Pos.Bills.Count == 2);
+            foreach (var item in PosMultiItem)
+            {
+                // count should be 1 for items3 and items4
+                Assert.IsTrue(item.Key.Bills.Count == 2 || item.Key.Bills.Count == 1);
+            }
+            foreach (var item in PosSingleItem)
+            {
+                // count should be 1 for items3 and items4
+                Assert.IsTrue( item.Key.Bills.Count == 1);
+            }
+
         }
 
         /// <summary>
@@ -28,8 +54,12 @@ namespace UnitTestPointOfSale
         [TestMethod]
         public void TestPOSBillAmount()
         {
-            double totalCost = Pos.Bills.Sum(p => p.Cost);
-            Assert.IsTrue(totalCost == 2.05);
+            var Pos = PosSingleItem.Concat(PosMultiItem);
+            foreach (var item in Pos)
+            {
+                double totalCost = item.Key.Bills.Sum(p => p.Cost);
+                Assert.IsTrue(totalCost == item.Value);
+            }
         }
 
         /// <summary>
@@ -38,8 +68,12 @@ namespace UnitTestPointOfSale
         [TestMethod]
         public void TestPOSBillAmountWithOffer()
         {
-            double totalCost = Pos.Bills.Sum(p => p.Cost);
-            Assert.IsTrue(totalCost == 1.45);
+            var Pos = PosSingleItem.Concat(PosMultiItem);
+            foreach (var item in Pos)
+            {
+                double totalCost = item.Key.Bills.Sum(p => p.Cost);
+                Assert.IsTrue(totalCost == item.Value);
+            }
         }
     }
 }
