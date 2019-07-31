8d3b2639b9317d18dfa993b7362c702b0af46ef7
TojThank <toji.pv@hotmail.com>
2019-07-31 15:46:27 +0100

    1) Updated PointOfSale with shoppingcart and creating list of bills

--- ShoppingCart/PointOfSale.cs
+++ ShoppingCart/PointOfSale.cs
@@ -5,10 +5,53 @@ using System.Text;
 namespace ShoppingCart
 {
     /// <summary>
-    /// Class point fo sale used to fetch the shopping cart and create bill
+    /// Class point of sale used to fetch the shopping cart and create bill
     /// </summary>
     public class PointOfSale
     {
-        public List<Bill> Bills { get; set; }
+        private Dictionary<string, int> shoppingCart;
+        private List<Bill> bills;
+
+        /// <summary>
+        /// Fetch the bills from shoping cart
+        /// </summary>
+        public List<Bill> Bills {
+            get
+            {
+                if (bills == null)
+                {
+                    bills = new List<Bill>();
+                    foreach (var item in shoppingCart)
+                    {
+                        switch (item.Key)
+                        {
+                            case "Apple":
+                                bills.Add(new Bill(new Apple(), item.Value));
+                                break;
+                            case "Orange":
+                                bills.Add(new Bill(new Orange(), item.Value));
+                                break;
+                        }
+                    }
+                }
+                return bills;
+            }
+        }
+
+        /// <summary>
+        /// Create the PointOfSale object and intailizes Shoppingcart from the string array
+        /// </summary>
+        /// <param name="cartItems"> string array of items </param>
+        public PointOfSale(string[] cartItems)
+        {
+            shoppingCart = new Dictionary<string, int>();
+            foreach (string fruit in cartItems)
+            {
+                if (shoppingCart.ContainsKey(fruit))
+                    shoppingCart[fruit] += 1;
+                else
+                    shoppingCart.Add(fruit, 1);
+            }
+        }
     }
 }
