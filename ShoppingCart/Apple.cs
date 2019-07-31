8f604c2c5f644c764ffd2849067fc5693cb057e5
TojThank <toji.pv@hotmail.com>
2019-07-31 15:38:29 +0100

    Added classes for Apple and Orange

---
+++ ShoppingCart/Apple.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart
+{
+    public class Apple : Fruit
+    {
+        public Apple() : base("Apple")
+        {
+
+        }
+        /// <summary>
+        /// Cost of single Apple
+        /// </summary>
+        public override double Cost { get => 0.60; }
+    }
+}
---
+++ ShoppingCart/Orange.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart
+{
+    public class Orange : Fruit
+    {
+        public Orange() : base("Orange")
+        {
+
+        }
+        /// <summary>
+        /// Cost of single Orange
+        /// </summary>
+        public override double Cost { get => 0.25; }
+    }
+}
